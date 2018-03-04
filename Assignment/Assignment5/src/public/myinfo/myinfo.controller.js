ada65146701b5c0b4e6ac41f4f4cb3f4fc7e12db
akash <akoshta16@gmail.com>
2018-03-04 10:48:25 +0530

    Assignment-5.6

--- Assignment/Assignment5/src/public/myinfo/myinfo.controller.js
+++ Assignment/Assignment5/src/public/myinfo/myinfo.controller.js
@@ -5,12 +5,13 @@
 angular.module('public')
 .controller('MyInfoController',MyInfoController);
 
-MyInfoController.$inject=['SignUpService','MenuService'];
-function MyInfoController(SignUpService,MenuService){
+MyInfoController.$inject=['SignUpService','MenuService','ApiPath'];
+function MyInfoController(SignUpService,MenuService,ApiPath){
 
  var ctrl = this;
  ctrl.user=SignUpService.getUser();
  ctrl.dish= MenuService.getShortNameD();
+ ctrl.basePath=ApiPath;
 }
 
 })();
